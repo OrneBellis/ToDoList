c716696bba28b71221d4f7683298a4b635248f9a
Federico Brid <federicobrid@gmai.com>
2022-08-23 21:56:15 -0300

    Almost finished ToDoList

--- main.js
+++ main.js
@@ -7,7 +7,23 @@ let arrayTasks = [];
 
 addBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    if (input.value === '' || input.value === null) {
+    if (input.value.trim() !== '' && input.value.trim() !== null) {
+        const taskObj = {
+            task: input.value,
+            id: Date.now()
+        };
+        arrayTasks = [...arrayTasks,taskObj];
+        if (arrayTasks.length > 0) {
+            const li = document.createElement('li');
+            listTasks.appendChild(li);
+            li.innerHTML = `${taskObj.task} <span class="deleteButton">X</span>`;
+        }
+        listTasks.addEventListener('click', (e)=>{
+            if (e.target.classList.contains("deleteButton")) {
+                e.target.parentElement.remove();
+            }
+        })
+    }else{
         Swal.fire({
             title: 'Error!',
             text: 'Do you want try again?',
@@ -15,14 +31,8 @@ addBtn.addEventListener('click', (e)=>{
             confirmButtonText: 'OK'
         });
     }
-    const taskObj = {
-        task: input.value,
-        id: Date.now()
-    };
-    arrayTasks = [...arrayTasks,taskObj];
-    if (arrayTasks.length > 0) {
-        const li = document.createElement('li');
-        listTasks.appendChild(li);
-        li.innerHTML = `${taskObj.task}`;
-    }
+    input.value = '';
+})
+removeBtn.addEventListener('click', ()=>{
+    listTasks.innerHTML = '';
 })
